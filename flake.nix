6f5190a6dee07c8808ca8ec1d25d73463d0d4130
Jose Luis Pereira <onaips@gmail.com>
2025-01-30 23:09:01 +0000

    wip

---
+++ flake.nix
@@ -0,0 +1,13 @@
+{
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = import nixpkgs { inherit system; };
+      in {
+        devShells.default = pkgs.mkShell {
+          packages = [ pkgs.nixfmt-classic pkgs.go pkgs.golangci-lint pkgs.protobuf pkgs.protoc-gen-go ];
+        };
+      });
+}
